c4feb148476ecf0dc7ff2ae37050707ae927fc6d
bobychaudhary <bobychaudhary708@gmail.com>
2021-12-06 19:17:46 +0530

    inorder traversal has been added

--- Rough/rough4.java
+++ Rough/rough4.java
@@ -52,3 +52,38 @@ class Solution {
         
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int grid(int[][] grid) {
+//         int m=grid.length, n=grid[0].length;
+//         if(grid[0][0]==1) return 0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 //first row && first column means boundary case
+//                 if(i==0 || j==0){
+//                     //its prev cell was obstable or  if current cell is obstacle
+//                     if(grid[i][j]=1 || (i!=0 && grid[i-1][0]==0)|| (j!=0 && grid[i][j-1]==0)){
+//                         grid[i][j]=0;
+//                     }
+//                     else{
+//                         grid[i][j]=1;
+//                     }
+//                 }
+//                 //all the other cells 
+//                 else{
+//                     if(grid[i][j]==1){
+//                         grid[i][j]=0;
+//                     }
+//                     else{
+//                         grid[i][j]=grid[i-1][j]+grid[i][j-1];                    }
+//                 }
+//             }
+//         }
+//         return grid[m-1][n-1];
+        
+//     }
+// }
---
+++ Trees/Recursive_inOrderTraversal.java
@@ -0,0 +1,4 @@
+// //recursive in orderr binary trree traversal
+// traverrse the lefft subtree in an in order fasion 
+// visit the root node
+// traverse the right subtree in an in order fasion
